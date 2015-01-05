5287930f24c42ca37da17abe1ecf7aec4767554a
AP Fritts <ap@apfritts.com>
2015-01-04 22:22:47 -0800

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by AP Fritts on 1/4/15.
+//  Copyright (c) 2015 AP Fritts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by AP Fritts on 1/4/15.
+//  Copyright (c) 2015 AP Fritts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
