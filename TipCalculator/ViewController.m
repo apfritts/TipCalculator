a822019a24041432896b58fc3ac45e92c7178eaf
AP Fritts <ap@apfritts.com>
2015-01-05 08:08:11 -0800

    Initial working version

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -9,6 +9,15 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billAmount;
+@property (weak, nonatomic) IBOutlet UILabel *tipAmount;
+@property (weak, nonatomic) IBOutlet UILabel *totalAmount;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipPercent;
+
+- (IBAction)onTap:(id)sender;
+- (IBAction)onEdit:(id)sender;
+
+-(void)updateValues;
 
 @end
 
@@ -22,6 +31,27 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:YES];
+    [self updateValues];
+}
+
+- (IBAction)onEdit:(id)sender {
+    // Change settings button to "Done"
+}
+
+-(void)updateValues {
+    float bill = [self.billAmount.text floatValue];
+    NSArray *tipValues = @[@(0.1), @(0.15), @(0.2)];
+    float tipPercent = [tipValues[self.tipPercent.selectedSegmentIndex] floatValue];
+    
+    float tip = bill * tipPercent;
+    float total = tip + bill;
+    
+    self.tipAmount.text = [NSString stringWithFormat:@"$%0.2f", tip];
+    self.totalAmount.text = [NSString stringWithFormat:@"$%0.2f", total];
 }
 
 @end
